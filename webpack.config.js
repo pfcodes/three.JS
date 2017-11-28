b940a4ef6226db0cce2605c37937db5d4e13f724
Phil Fevry <pf@phlfvry.com>
2017-11-28 13:09:24 -0500

    added sass loader to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,18 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-  entry: './scripts/app.js',
+  entry: ['./scripts/app.js', './scss/main.scss'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public')
   },
   module: {
     rules: [{
-      test: /\.scss$/,
+      test: /\.scss$/, // TODO: Before production add required plugins
       use: [{
-        loader: 'style-loader',
+        loader: 'style-loader'
       }, {
-        loader: 'css-loader',
+        loader: 'css-loader'
       }, {
         loader: 'sass-loader'
       }]
